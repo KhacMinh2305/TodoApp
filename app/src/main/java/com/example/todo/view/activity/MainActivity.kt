827303d09cfcb8a1f538cc4a35a32eb98acb9946
Nguyen Duy <ngduy25062003@gmail.com>
2024-11-15 00:17:19 +0700

    create model database

--- app/src/main/java/com/example/todo/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/todo/view/activity/MainActivity.kt
@@ -1,6 +1,7 @@
-package com.example.todo
+package com.example.todo.view.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.todo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
